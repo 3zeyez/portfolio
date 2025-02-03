9269fed4461638785b2c67ccc58fac52f7cf3dca
3zyez <77698923+3zyez@users.noreply.github.com>
2025-02-03 03:01:34 +0100

    Fix: web page title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <title>Ahmed Aziz Abbassi | Software Developer</title>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
